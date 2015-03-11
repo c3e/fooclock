67acb6f2e83d09be4bec49b8b9785a1bd408ea96
Merlin Raschtuttis <merlin.raschtuttis@hs-bochum.de>
2015-03-11 21:29:11 +0100

    Fixed Booting-Text. Remember to write Messages backwards, when initializing the array. Some improvements to the letters.

--- fooclock.ino
+++ fooclock.ino
@@ -4,6 +4,36 @@
 #include <SPI.h>
 #include <TimerOne.h>
 
+#define A 0
+#define B 1
+#define C 2
+#define D 3
+#define E 4
+#define F 5
+#define G 6
+#define H 7
+#define I 8
+#define J 9
+#define K 10
+#define L 11
+#define M 12
+#define N 13
+#define O 14
+#define P 15
+#define Q 16
+#define R 17
+#define S 18
+#define T 19
+#define U 20
+#define V 21
+#define W 22
+#define X 23
+#define Y 24
+#define Z 25
+
+
+
+
 
 byte mac[] = {
 	0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED };
@@ -20,15 +50,17 @@ const int timeZone = 1;     // Central European Time
 //const int timeZone = -7;  // Pacific Daylight Time (USA)
 
 time_t cur_time     = 0;
+time_t alarm_goal	= 0;
 bool update_done    = false;
-int cur_sec         = 0;
-int cur_min         = 0;
+bool second_changed = false;
 int cur_update      = 0;
 int shift_state     = 0;
-int update_counter  = 0;
+int update_counter	= 0;
 
 EthernetUDP Udp;
-unsigned int localPort = 8888;  // local port to listen for UDP packets
+EthernetUDP AlarmClock;
+unsigned int alarmClockPort = 123; // local port to listen for custom AlarmClock packets
+unsigned int localPort		= 8888;  // local port to listen for UDP packets
 
 int OutputEnable    = 3;
 
@@ -52,14 +84,6 @@ int dataPin         = 5;
 //     ||  64  ||
 //      \======/ O  1
 
-
-int frame [6] = {
-	124,126,174,144,126,0 // Display "Start"
-};
-int today [6] = {
-	184,220,200,200,240,0 // Display "Hello"
-};
-
 int duration = 10;
 
 int digits[10] = {
@@ -77,7 +101,7 @@ int digits[10] = {
 
 int letter [26] = {
 
-	174, // A  
+	191, // A  
 	248, // b
 	204, // C
 	242, // d 
@@ -96,7 +120,7 @@ int letter [26] = {
 	238, // Q  
 	144, // r  
 	124, // S  
-	126, // t  
+	216, // t  
 	224, // u  
 	224, // v  
 	26,  // w  
@@ -104,6 +128,14 @@ int letter [26] = {
 	58,  // y 
 	214, // Z
 };
+
+int frame [6] = {
+	0,letter[T],letter[R],letter[A],letter[T],letter[S] // Display "Start"
+};
+int today [6] = {
+	184,220,200,200,240,0 // Display "Hello"
+};
+
 
 #define UP		1
 #define DOWN	2
@@ -119,6 +151,7 @@ void setup()
 
 	Timer1.initialize(1000);  // initialize timer1, and set a 1 milli second period
 	Timer1.attachInterrupt(updateDisplay);  // attaches callback() as a timer overflow interrupt
+	updateDisplay();
 
 	Serial.begin(9600);
 
@@ -137,6 +170,13 @@ void loop()
 	//int test [] = {184,220,200,200,240,0}; // Display "Hello"
 
 	time_t timestamp = now();
+	//Sync blinking with second change
+	if(cur_time != timestamp && !second_changed){
+		update_counter = 0;
+		cur_time = timestamp;
+		second_changed = true;
+	}
+
 	int intervalpos = timestamp%70;
 	if( intervalpos == 0){
 		update_done = false;
@@ -157,12 +197,16 @@ void loop()
 	}
 
 	// Blinkenfoo - comment to remove pulsating light
-	if(update_counter < 200){
-		dim(UP);
+	if(update_counter < 125  && timestamp%600 == 0 ){
+		dim(UP,125,255);
 	}
-	if(update_counter > 800){
-		dim(DOWN);
+	if(update_counter > 875  && timestamp%600 == 0 ){
+		dim(DOWN,125,255);
 	}
+	if(update_counter == 900){
+		second_changed = true;
+	}
+
 
 }
 
@@ -178,11 +222,16 @@ void updateDate(time_t t)
 	today[5] = digits[((year(t)-2000) %10)];
 }
 
-void dim(int direction){
-	if(direction == DOWN){
-		analogWrite(OutputEnable, update_counter % 200);
+void dim(int direction, int lower_limit, int upper_limit){
+	int range = upper_limit - lower_limit;
+	if(direction == DOWN && update_counter % range <= range ){
+		analogWrite(OutputEnable, lower_limit + (update_counter % range));
+		Serial.println("Down ->");
+		Serial.println(lower_limit+update_counter % range);
 	} else{
-		analogWrite(OutputEnable, 200 - update_counter % 200);
+		analogWrite(OutputEnable, upper_limit - (update_counter % range));
+		Serial.println("UP ->");
+		Serial.println(upper_limit - update_counter % range);
 	}
 }
 
@@ -199,7 +248,6 @@ void transition(int* a){
 	if (update_counter % 300 == 0 && cur_update != update_counter && shift_state < sizeof(today)/2){		//assuming the Array contains only ints, 
 																										//which have a sizeof 2 on this Arduino
 		Serial.println(sizeof(a));
-		cur_sec     = second(t);
 		cur_update  = update_counter;
 		shift_right(a[5-shift_state]);
 		shift_state ++;
@@ -272,6 +320,26 @@ void displayBinaryTime(time_t t)
 }
 
 
+/*-------- Alarm Clock code ---------*/
+const int AlarmClockPacketSize = 256;
+byte AlarmBuffer[AlarmClockPacketSize];
+
+int listenForAlarm(){
+	while (AlarmClock.parsePacket() > 0);
+	Serial.println("Any Alarm imminent?");
+	uint32_t packageBegin = millis();
+	while(millis() - packageBegin < 1500){
+		int size = AlarmClock.parsePacket();
+		if(size >= AlarmClockPacketSize){
+			Serial.println("Alarm seems to be imminent.");
+			AlarmClock.read(AlarmBuffer, AlarmClockPacketSize);
+			alarm_goal = (unsigned long)AlarmBuffer;
+			return 1;
+		}
+	}
+	Serial.println("No Alarm present!");
+	return 0;
+}
 
 /*-------- NTP code ----------*/
 
@@ -281,7 +349,7 @@ byte packetBuffer[NTP_PACKET_SIZE]; //buffer to hold incoming & outgoing packets
 time_t getNtpTime()
 {
 	while (Udp.parsePacket() > 0) ; // discard any previously received packets
-	Serial.println("Transmit NTP Request");
+	//Serial.println("Transmit NTP Request");
 	sendNTPpacket(timeServer);
 	uint32_t beginWait = millis();
 	while (millis() - beginWait < 1500) {
@@ -298,7 +366,7 @@ time_t getNtpTime()
 			return secsSince1900 - 2208988800UL + timeZone * SECS_PER_HOUR;
 		}
 	}
-	Serial.println("No NTP Response :-(");
+	//Serial.println("No NTP Response :-(");
 	return 0; // return 0 if unable to get the time
 }
 
